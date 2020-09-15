f391e5babefb8856b642d1659e4418f891dd3547
Yang Luo <992534544@qq.com>
2020-09-15 16:51:48 +0800

    commit before synchronization

--- examples/com/kuka/connectivity/fri/example/LBRWrenchSineOverlay.java
+++ examples/com/kuka/connectivity/fri/example/LBRWrenchSineOverlay.java
@@ -35,7 +35,7 @@ public class LBRWrenchSineOverlay extends RoboticsAPIApplication
         // **********************************************************************
         // *** change next line to the FRIClient's IP address                 ***
         // **********************************************************************
-        _clientName = "127.0.0.1";
+        _clientName = "172.31.1.100";
     }
 
     @Override
